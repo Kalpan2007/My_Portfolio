affe807b32395cd9094b69b13c87d31679b8333f
Kalpan2007 <kalpankaneriya@gmail.com>
2025-05-16 10:39:52 +0530

    Chnage Config To Run Ngrok

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,15 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-
 export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
+  },
+  server: {
+    host: true,
+    port: 5173,
+    allowedHosts: ['.ngrok-free.app', 'localhost', '127.0.0.1'], 
+    // allow all ngrok subdomains ending with .ngrok-free.app
   },
 });
