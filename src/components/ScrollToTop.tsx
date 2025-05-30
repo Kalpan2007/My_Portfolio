69aea4ad39c98ac3f1fd52adc0c4fcdf67275be6
Kalpan2007 <kalpankaneriya@gmail.com>
2025-05-30 18:26:26 +0530

    Fix On Top Code

--- src/components/ScrollToTop.tsx
+++ src/components/ScrollToTop.tsx
@@ -1,14 +1,23 @@
 import { useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
 
-const ScrollToTop = () => {
+export default function ScrollToTop() {
   const { pathname } = useLocation();
 
   useEffect(() => {
-    window.scrollTo(0, 0);
+    const scrollOptions = {
+      top: 0,
+      left: 0,
+      behavior: 'smooth' as const
+    };
+
+    // Delay scroll to top to allow for page transition animations
+    const timeoutId = setTimeout(() => {
+      window.scrollTo(scrollOptions);
+    }, 100);
+
+    return () => clearTimeout(timeoutId);
   }, [pathname]);
 
   return null;
-};
-
-export default ScrollToTop;
+}
